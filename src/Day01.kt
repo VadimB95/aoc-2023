187a46d9dbcf3ba3b31515357fabf3fa0ae55847
v.babikov <v.babikov@tinkoff.ru>
2023-12-01 14:29:44 +0400

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,52 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    check(part2(readInput("Day01_part2_test")) == 281)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun part1(input: List<String>): Int {
+    return input.sumOf { line ->
+        buildString {
+            append(line.first { it.isDigit() })
+            append(line.last { it.isDigit() })
+        }.toInt()
+    }
+}
+
+private fun part2(input: List<String>): Int {
+    return input.sumOf { line ->
+        val filteredDigits = buildString {
+            for (i in line.indices) {
+                if (line[i].isDigit()) {
+                    append(line[i])
+                } else {
+                    val substring = line.substring(i)
+                    for (digit in Digits.entries) {
+                        if (substring.startsWith(digit.name, true)) {
+                            append(digit.stringDigit)
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        "${filteredDigits.first()}${filteredDigits.last()}".toInt()
+    }
+}
+
+private enum class Digits(val stringDigit: String) {
+    ONE("1"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9")
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,11 +2,17 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.io.path.readText
 
 /**
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = Path("src/$name.txt").readLines()
+
+/**
+ * Reads the given input txt file as string
+ */
+fun readInputAsString(name: String) = Path("src/$name.txt").readText()
 
 /**
  * Converts string to md5 hash.
